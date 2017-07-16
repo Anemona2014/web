a01041fa9fd75a066aff21798ca8768a0ee0fab0
git@github.com:Anemona2014 <e.menkovich@netrika.ru>
2017-07-16 19:13:26 +0300

    add custom manager

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -2,9 +2,18 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.db.models import Max
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+class QuestionManager(models.Manager):
+
+    def new(self):
+        return self.last()
+
+    def popular(self):
+        return self.objects.all().aggregate(Max('likes'))
 
 class Question(models.Model):
     title = models.CharField(max_length=255)
@@ -13,6 +22,8 @@ class Question(models.Model):
     rating = models.ImageField()
     author = models.ForeignKey(User)
     likes = models.ManyToManyField(User, related_name='likes')
+
+    objects = QuestionManager()
 
     def __str__(self):
         return self.title
